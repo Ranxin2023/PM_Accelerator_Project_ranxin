7210de9e4f380859363687d3438dfbbe083d01b5
Ranxin Li <ranxinli2024@gmail.com>
2025-07-09 01:15:13 -0700

    update local storage for user context

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,7 +1,20 @@
-import { createContext, useContext, useState } from "react";
-export const UserContext=createContext()
+import { createContext, useContext, useState, useEffect } from "react";
+
+const UserContext = createContext();
+
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // e.g. { email: "..." }
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
